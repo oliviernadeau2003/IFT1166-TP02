f551886866bbe4022b01b4faf0d53c936d19089a
Olivier <oliviernadeau2003@gmail.com>
2024-11-19 23:15:58 -0500

    .

--- TP2.cpp
+++ TP2.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <iostream>
+#include "classe/Epice.cpp"
 using namespace std;
 
 int main()
 {
-    printf("Hello World");
-    return 1;
+    Epice myEpice = Epice(8, "Cardamome", "Graines", 10.0, 60.0, "Épices");
+
+    myEpice.afficher();
+
+    system("pause"); //! REMOVE LATER
+    return 0;
 }
--- classe/Epice.cpp
+++ classe/Epice.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 using namespace std;
 
 // 1;Cumin;Graines;2.5;100.0;Épices;1;2
@@ -11,8 +12,15 @@ private:
     string type;
     double prix;
     double quantite;
+    string categorie;
 
 public:
-    Epice() : id(0), nom(""), type(""), prix(0.0), quantite(0.0) {};
-    Epice(int id, string nom, string type, double prix, double quantite) : id(id), nom(nom), type(type), prix(prix), quantite(quantite) {}
+    Epice() : id(0), nom(""), type(""), prix(0.0), quantite(0.0), categorie("") {}
+    Epice(int id, string nom, string type, double prix, double quantite, string categorie)
+        : id(id), nom(nom), type(type), prix(prix), quantite(quantite), categorie(categorie) {};
+
+    void afficher() const
+    {
+        cout << "ID: " << this->id << " Nom: " << this->nom << " Type: " << this->type << " Prix: " << this->prix << " Quantité: " << this->quantite << " Catégorie: " << this->quantite << "\n";
+    }
 };
